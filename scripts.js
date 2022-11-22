74882deb2f1acd48999bf61ee317383e9d11ad0b
SantyRodriguez <santiagorodriguez8007@gmail.com>
2022-11-22 16:51:29 -0500

    update

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.icon')
+const menun = document.querySelector('.na_l')
+
+hamburger.addEventListener('click', () => {
+    menun.classList.toggle('activo')
+
+})
